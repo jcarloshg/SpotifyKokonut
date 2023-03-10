fb540cdba69da55ddb24565dba6f16525ae6d7f5
jcarloshg <carlosj12336@gmail.com>
2023-03-09 18:52:47 -0600

    create componen [HeaderComponent]

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
 import { TitleComponent } from './title/title.component';
 import { SubtitleComponent } from './subtitle/subtitle.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     PrimaryButtonComponent,
     SubtitleComponent,
     TitleComponent,
+    HeaderComponent,
   ],
   imports: [
     CommonModule
@@ -17,6 +19,7 @@ import { SubtitleComponent } from './subtitle/subtitle.component';
     PrimaryButtonComponent,
     SubtitleComponent,
     TitleComponent,
+    HeaderComponent,
   ]
 })
 export class ComponentsModule { }
