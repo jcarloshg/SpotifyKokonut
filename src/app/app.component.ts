66ee85d18bca5b48d34c7175a35c1caf7caa56d0
jcarloshg <carlosj12336@gmail.com>
2023-03-10 01:26:08 -0600

    create inplement [ngOnInit]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'spotify-kokonut';
 
+  constructor(private activatedRoute: ActivatedRoute, private location: Location) { }
+
+  ngOnInit(): void {
+    const plok = new URLSearchParams(this.location.path());
+    console.log(plok.get("code"));
+
+    console.log(this.location.path());
+
+
+  }
+
   goToSpotifySingUp() {
     // document.location.href = 'https://www.spotify.com/mx/signup';
     window.open("https://www.spotify.com/mx/signup")
   }
 
-  goToPlok() {
-    // document.location.href = 'https://stackoverflow.com';
-    window.open("https://www.spotify.com/mx/signup")
+  goLogin() {
+    window.open("https://accounts.spotify.com/authorize?response_type=code&client_id=3503c48c3bd148ba9bddfce19674184b&scope=user-read-private%20user-read-email&redirect_uri=http://localhost:4200/");
+
+    // https://accounts.spotify.com/authorize?response_type=code&client_id=3503c48c3bd148ba9bddfce19674184b&scope=user-read-private%20user-read-email&redirect_uri=http://localhost:4200/&state=202020
   }
 }
