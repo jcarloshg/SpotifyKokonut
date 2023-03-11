396067373f9def1d37e27059abd4401db5e48ace
jcarloshg <carlosj12336@gmail.com>
2023-03-11 12:43:40 -0600

    create && implement page [SearcherComponent] into routing-module [view-content-routing.module.ts]

--- src/app/pages/view-content/view-content.module.ts
+++ src/app/pages/view-content/view-content.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { ViewContentRoutingModule } from './view-content-routing.module';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
+import { SearcherComponent } from './pages/searcher/searcher.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SearcherComponent
   ],
   imports: [
     CommonModule,
