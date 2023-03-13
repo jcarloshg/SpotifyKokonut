d9373bdad96ccb18f920c36f138768c36462d861
jcarloshg <carlosj12336@gmail.com>
2023-03-13 14:49:31 -0600

    modify html from component [AlbumDetailsComponent]

--- src/app/pages/view-content/view-content-routing.module.ts
+++ src/app/pages/view-content/view-content-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearcherComponent } from './pages/searcher/searcher.component';
-import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
+import { AlbumDetailsComponent } from './pages/album-details/pages/album-details/album-details.component';
+
 
 const routes: Routes = [
   {
@@ -30,36 +31,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class ViewContentRoutingModule { }
-
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from './pages/home/home.component';
-// import { SearcherComponent } from './pages/searcher/searcher.component';
-// import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
-
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: HomeComponent,
-//     children: [
-//       {
-//         path: 'buscador',
-//         component: SearcherComponent,
-//       },
-//       {
-//         path: 'album/:id',
-//         component: AlbumDetailsComponent,
-//       },
-//       {
-//         path: '**',
-//         redirectTo: 'buscador',
-//       }
-//     ],
-//   }
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forChild(routes)],
-//   exports: [RouterModule]
-// })
-// export class ViewContentRoutingModule { }
--- src/app/pages/view-content/view-content.module.ts
+++ src/app/pages/view-content/view-content.module.ts
@@ -8,7 +8,7 @@ import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { SearcherComponent } from './pages/searcher/searcher.component';
 import { SearcherAndListItemsModule } from './pages/searcher/components/searcher-and-list-items/searcher-and-list-items.module';
 import { ImageBackgroundComponent } from './pages/searcher/components/image-background/image-background.component';
-import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
+import { AlbumDetailsModule } from './pages/album-details/album-details.module';
 
 
 @NgModule({
@@ -16,7 +16,7 @@ import { AlbumDetailsComponent } from './pages/album-details/album-details.compo
     HomeComponent,
     SearcherComponent,
     ImageBackgroundComponent,
-    AlbumDetailsComponent,
+    // AlbumDetailsComponent,
   ],
   imports: [
     CommonModule,
@@ -25,41 +25,9 @@ import { AlbumDetailsComponent } from './pages/album-details/album-details.compo
     ComponentsModule,
     // mine
     SearcherAndListItemsModule,
+    AlbumDetailsModule,
   ],
   providers: [
   ]
 })
 export class ViewContentModule { }
-
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// // mine
-// import { HomeComponent } from './pages/home/home.component';
-// import { ViewContentRoutingModule } from './view-content-routing.module';
-// import { ComponentsModule } from 'src/app/shared/components/components.module';
-// import { SearcherComponent } from './pages/searcher/searcher.component';
-// import { SearcherAndListItemsModule } from './pages/searcher/components/searcher-and-list-items/searcher-and-list-items.module';
-// import { ImageBackgroundComponent } from './pages/searcher/components/image-background/image-background.component';
-// import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
-
-
-// @NgModule({
-//   declarations: [
-//     HomeComponent,
-//     SearcherComponent,
-//     ImageBackgroundComponent,
-//     AlbumDetailsComponent,
-//   ],
-//   imports: [
-//     CommonModule,
-//     FormsModule,
-//     ViewContentRoutingModule,
-//     ComponentsModule,
-//     // mine
-//     SearcherAndListItemsModule,
-//   ],
-//   providers: [
-//   ]
-// })
-// export class ViewContentModule { }
