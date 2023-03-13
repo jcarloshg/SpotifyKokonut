55edf5fc406a19cd247c9b19c44ff34cbfe5d897
jcarloshg <carlosj12336@gmail.com>
2023-03-13 10:37:33 -0600

    save

--- src/app/pages/view-content/view-content-routing.module.ts
+++ src/app/pages/view-content/view-content-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearcherComponent } from './pages/searcher/searcher.component';
+import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'buscador',
         component: SearcherComponent,
+      },
+      {
+        path: 'album/:id',
+        component: AlbumDetailsComponent,
       },
       {
         path: '**',
@@ -25,3 +30,36 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class ViewContentRoutingModule { }
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './pages/home/home.component';
+// import { SearcherComponent } from './pages/searcher/searcher.component';
+// import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: HomeComponent,
+//     children: [
+//       {
+//         path: 'buscador',
+//         component: SearcherComponent,
+//       },
+//       {
+//         path: 'album/:id',
+//         component: AlbumDetailsComponent,
+//       },
+//       {
+//         path: '**',
+//         redirectTo: 'buscador',
+//       }
+//     ],
+//   }
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule]
+// })
+// export class ViewContentRoutingModule { }
--- src/app/pages/view-content/view-content.module.ts
+++ src/app/pages/view-content/view-content.module.ts
@@ -8,6 +8,7 @@ import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { SearcherComponent } from './pages/searcher/searcher.component';
 import { SearcherAndListItemsModule } from './pages/searcher/components/searcher-and-list-items/searcher-and-list-items.module';
 import { ImageBackgroundComponent } from './pages/searcher/components/image-background/image-background.component';
+import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { ImageBackgroundComponent } from './pages/searcher/components/image-back
     HomeComponent,
     SearcherComponent,
     ImageBackgroundComponent,
+    AlbumDetailsComponent,
   ],
   imports: [
     CommonModule,
@@ -28,3 +30,36 @@ import { ImageBackgroundComponent } from './pages/searcher/components/image-back
   ]
 })
 export class ViewContentModule { }
+
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// // mine
+// import { HomeComponent } from './pages/home/home.component';
+// import { ViewContentRoutingModule } from './view-content-routing.module';
+// import { ComponentsModule } from 'src/app/shared/components/components.module';
+// import { SearcherComponent } from './pages/searcher/searcher.component';
+// import { SearcherAndListItemsModule } from './pages/searcher/components/searcher-and-list-items/searcher-and-list-items.module';
+// import { ImageBackgroundComponent } from './pages/searcher/components/image-background/image-background.component';
+// import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
+
+
+// @NgModule({
+//   declarations: [
+//     HomeComponent,
+//     SearcherComponent,
+//     ImageBackgroundComponent,
+//     AlbumDetailsComponent,
+//   ],
+//   imports: [
+//     CommonModule,
+//     FormsModule,
+//     ViewContentRoutingModule,
+//     ComponentsModule,
+//     // mine
+//     SearcherAndListItemsModule,
+//   ],
+//   providers: [
+//   ]
+// })
+// export class ViewContentModule { }
