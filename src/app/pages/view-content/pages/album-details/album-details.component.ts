6171783add049331ba34abebf8f7b98d31f2b058
jcarloshg <carlosj12336@gmail.com>
2023-03-13 10:35:04 -0600

    create && implement route [goToAlbumDetailsPage]

--- src/app/pages/view-content/pages/album-details/album-details.component.ts
+++ src/app/pages/view-content/pages/album-details/album-details.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './album-details.component.html',
   styleUrls: ['./album-details.component.css']
 })
-export class AlbumDetailsComponent { }
+export class AlbumDetailsComponent {
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log('====================================');
+    console.log('hola');
+    console.log('====================================');
+  }
+}
